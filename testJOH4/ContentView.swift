076f927777ecf6f690ba57b90dca18f9ca264444
johalle48 <gabmarsol@gmail.com>
2025-03-24 17:30:35 +0100

    V3

--- testJOH4/ContentView.swift
+++ testJOH4/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "globeV3")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!V3")
         }
         .padding()
     }
